cd7bfc585728856d89d10d0f6e7f5e394f690157
Jack Schuss <jackschuss@gmail.com>
2016-04-12 22:26:14 -0400

    fix db

--- db/migrate/20160411180135_create_ticket_types.rb
+++ db/migrate/20160411180135_create_ticket_types.rb
@@ -1,8 +1,8 @@
 class CreateTicketTypes < ActiveRecord::Migration
   def change
     create_table :ticket_types do |t|
-      t.integer :cost
-      t.string :type
+      t.decimal :cost
+      t.string :level
       t.integer :event_id
       t.timestamps null: false
     end
